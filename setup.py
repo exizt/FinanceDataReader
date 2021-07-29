516c63f02b93233b8da953e958466374d7daf5c2
SH.H <e2xist@gmail.com>
2021-07-30 04:31:13 +0900

    modify setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(name='FinanceDataReader-forked',
-      version='0.1',
+      version='0.9.30',
       packages=find_packages(),
       install_requires=[
             'pandas',
-            'tqdm',
+            'requests',
             'requests-file',
             'lxml',
-            'beautifulsoup4'
+            'tqdm'
       ])
